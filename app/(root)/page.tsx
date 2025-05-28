c7227785644d222207793788fc3f94700ebe58ec
chorus09 <thetrumer@gmail.com>
2025-05-28 22:12:22 +0300

    feat(ui) add dialog component from shadcn/ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
   return (
-    <div className="p-4">
-        <UserButton afterSignOutUrl="/"/>
+    <div className="flex items-center justify-center h-screen">
+      <Modal isOpen onClose={() => {}} title="Setup" description="Setup your account">
+        Children
+      </Modal>
     </div>
   )
 }
