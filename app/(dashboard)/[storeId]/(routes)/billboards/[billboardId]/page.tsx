8026e50015f16c5be8c0787f478ea1f0f5dcef5f
chorus09 <thetrumer@gmail.com>
2025-05-30 00:27:49 +0300

    fix(imports) bug: change the import for prismadb

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@/lib/generated/prisma";
+import prismadb from "@/lib/prismadb";
+
+
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+   const billboard = await prismadb.billboard?.findUnique({
+      where: { id: params.billboardId }
+   })
+
+   return (
+      <div>Existing Billboard: {billboard?.label}</div>
+   );
+};
+
+export default BillboardPage;
