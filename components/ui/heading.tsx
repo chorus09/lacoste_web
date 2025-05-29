cea02f54019eedc233a362133eadd486bfbe2477
chorus09 <thetrumer@gmail.com>
2025-05-29 19:26:45 +0300

    refactor(ui) simplify settings form

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -6,8 +6,10 @@ interface HeadingProps {
 export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
    return (
       <div>
-         <h1>{title}</h1>
-         <p>{description}</p>
+         <h1 style={{ fontSize: '2rem', margin: 0, fontWeight: 'bold' }}>{title}</h1>
+         {description && (
+         <p style={{ fontSize: '1rem', margin: 0, color: '#555' }}>{description}</p>
+         )}
       </div>
-   )
-}
+   );
+};
