d9a51b6254b15a385417629459d3a5811292849f
chorus09 <thetrumer@gmail.com>
2025-05-29 21:26:29 +0300

    fix(delete req) bug: delete request don't delete the store

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-from.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-from.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AlertModal } from "@/components/modals/alert-modal";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Heading } from "@/components/ui/heading";
@@ -51,8 +52,30 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
       }
    };
 
+   const onDelete = async () => {
+      try {
+         setLoading(true);
+         console.log(params.storeId)
+         await axios.delete(`${window.location.origin}/api/stores/${params.storeId}`);
+         router.refresh();
+         router.push("/");
+         toast.success("Store deleted.");
+      } catch(error) {
+         toast.error("Make sure you removed all products and categories first.")
+      } finally {
+         setLoading(false);
+         setOpen(false);
+      }
+   }
+
    return (
       <> 
+         <AlertModal 
+            isOpen={open}
+            onClose={() => setOpen(false)}
+            onConfirm={onDelete}
+            loading={loading}
+         />
          <div className="flex items-center justify-between">
             <Heading 
                title="Settings"
