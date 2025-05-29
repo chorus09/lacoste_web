83599fd72c6715f59d9d427359799faeb8acbef9
chorus09 <thetrumer@gmail.com>
2025-05-29 03:27:01 +0300

    refactor(ui) about active store

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,26 @@
-const DashboardPage = () => {
+import prismadb from "@/lib/prismadb";
+import React from "react";
+
+interface DashboardPageProps {
+   params: {
+      storeId: string;  
+   };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+   const store = await prismadb.store.findFirst({
+      where: {
+         id: params.storeId,
+      },
+   });
+
    return (
       <div>
-         This is a Dashboard page
+         Active Store: {store?.name || "Store not found"}
+         <br />
       </div>
-   );
-};
+   )
+}
+
 
 export default DashboardPage;
