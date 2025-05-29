a5fcdd8939993757fc0add055383d45baa2753d3
chorus09 <thetrumer@gmail.com>
2025-05-29 21:35:00 +0300

    fix(ui) change the style links on page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -5,29 +5,40 @@ import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 
 export function MainNav({
-   storeId,
-   className,
-   ...props
+  storeId,
+  className,
+  ...props
 }: { storeId: string } & React.HTMLAttributes<HTMLElement>) {
-   const pathname = usePathname();
-   const params = useParams();
+  const pathname = usePathname();
+  const params = useParams();
 
-   const routes = [{
+  const routes = [
+    {
+      href: `/${params.storeId}`,
+      label: "Overview",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
-   }];
+    },
+  ];
 
-   return (
-      <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
-         {routes.map((route) => (
-            <Link key={route.href} href={route.href} className={cn(
-               "text-sm font-medium transition-colors hover:text-primary",
-               route.active ? "text-black dark:text-white" : "text-muted-foreground",
-            )}>
-               {route.label}
-            </Link>
-         ))}
-      </nav>
-   )
+  return (
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6 pl-4", className)}>
+      {routes.map((route) => (
+        <Link
+          key={route.href + route.label}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active ? "text-black dark:text-white" : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
 }
