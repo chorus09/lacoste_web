783b272343da4050e379186a14334eb8a29eda47
chorus09 <thetrumer@gmail.com>
2025-06-06 23:57:59 +0300

    fix(route) bug: route to '/' after delete

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -74,7 +74,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
          console.log(params.storeId)
          await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
          router.refresh();
-         router.push("/");
+         router.push(`/${params.storeId}/billboards`);
          toast.success("Billboard deleted.");
       } catch(error) {
          toast.error("Make sure you removed all categories using this billboard first.")
@@ -150,7 +150,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
                </Button>
             </form>
          </Form>
-         <Separator />
       </>
    );
 };
